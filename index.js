a8c000712170ad5b9099c1af904b1d4cfbfd416b
clowboy-com <simone.fracassetti@clowboy.com>
2020-10-04 21:41:27 +0200

    Little code fixes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
- *  VERSION: @1.0.2
+ *  VERSION: @1.0.5
  *  PACKAGE: Antiflood
  *  LICENSE: DO WHAT EVER YOU WANT BUT DON'T RESELL IT
  *  AUTHOR: clowboy
@@ -13,10 +13,10 @@ module.exports = class{
 	}
 	feed(token = {}){
 		const currentDate = {timestamp: Math.floor(Date.now() / 1000), unique: token};
-		if(this.requests.filter(r => currentDate - r.timestamp < this.timeout && JSON.stringify(r.unique) === JSON.stringify(token)).length + 1 > this.count)
-			return false;
-		else
-			this.requests.push(currentDate);
-		return true;
+		this.requests.push(currentDate);
+		const spam = this.requests.filter(request => currentDate.timestamp - request.timestamp < this.timeout && JSON.stringify(request.unique) === JSON.stringify(token));
+		return !(spam.length > this.count);
 	}
 }
+
+// this.requests.filter(r => currentDate - r.timestamp < this.timeout && JSON.stringify(r.unique) === JSON.stringify(token))
